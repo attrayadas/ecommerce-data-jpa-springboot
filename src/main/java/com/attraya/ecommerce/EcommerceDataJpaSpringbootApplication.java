218cccaba91004e856e8caf494bf7f352f29348d
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-19 20:57:36 +0530

    Initial commit

---
+++ src/main/java/com/attraya/ecommerce/EcommerceDataJpaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.attraya.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDataJpaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDataJpaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attraya/ecommerce/EcommerceDataJpaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attraya.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDataJpaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
